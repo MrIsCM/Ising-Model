45a39f7d136f5208af450f64d84d408834643777
MrIsCM <61559771+MrIsCM@users.noreply.github.com>
2025-04-16 23:25:01 +0200

    Added a simulation

--- pruebas.py
+++ pruebas.py
@@ -1,8 +1,13 @@
+# Imports
 import numpy as np
 from IsingClass import IsingModel
 
+# Esta es la seed. Si quieres reproducibilidad, ponla siempre igual.
+# Si no te importa, puedes poner 'None'
 SEED = 42
 
+
+# Estos son los parametros del modelo clasico
 params_clasico = {
     'N' : 64,
     'T' : 1.0,
@@ -12,15 +17,17 @@ params_clasico = {
     'test_mode' : True
 }
 
+# Los parametros del modificado son iguales, solo hay que cambiar 'J2
 params_mod = params_clasico.copy()
 params_mod['J2'] = 2.0
 
 # Aqui defines el modelo
-modelo_clasico = IsingModel(**params_clasico)
-# modelo_mod = IsingModel(**params_mod)
+modelo_clasico = IsingModel(**params_clasico)   #este es el clasico
+modelo_mod = IsingModel(**params_mod)           #este es el modificado
 
-# Inicia la red
+# Inicia las redes: clasica y modificada
 modelo_clasico.inicia_red()
+modelo_mod.inicia_red()
 
 # Aqui configuras los parametros de la simulacion
 params_simulacion = {
@@ -28,19 +35,36 @@ params_simulacion = {
     'generar_imagenes' : True,
     'n_imagenes' : 5,
     'espaciado_imagenes' : 'lineal',
-    'generar_gif' : True,
-    'pasos_muestreo_frames' : 10,
+    # Si quieres el gif tienes que descomentar las dos siguientes lineas
+    # Y otras mas abajo indicadas igual.
+    # 'generar_gif' : True,
+    # 'pasos_muestreo_frames' : 10,
     'calcular_magnetizacion' : True,
     'pasos_muestreo_magnetizacion' : 5,
 }
 
 # Ejecuta la simulacion
 modelo_clasico.simular(**params_simulacion)
+modelo_mod.simular(**params_simulacion)
 
-# Configura los paths
+# Configura los paths (de momento puede dar algun error, tengo que cambiar un par de cosas)
 modelo_clasico.configurar_paths()
+modelo_mod.configurar_paths(
+    gif_dir='mod/gif',
+    imagenes_dir='mod/imagenes',
+    magnetizaciones_dir='mod/magnetizacion',
+)
 
-# Guardar cosas
+
+# Guardar cosas del modelo clasico
 modelo_clasico.guardar_imagenes()
-modelo_clasico.crear_gif()
 modelo_clasico.graficar_magnetizacion()
+# Si quieres el gif, descomenta la siguiente linea
+# modelo_clasico.crear_gif() 
+
+
+# Guardar cosas del modelo modificado
+modelo_mod.guardar_imagenes()
+modelo_mod.graficar_magnetizacion()
+# Igual que antes, si quieres el gif, descomenta la siguiente linea
+# modelo_mod.crear_gif()
