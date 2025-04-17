932c23d228669759ee4c076e3bc4d1d0eff7ed6a
MrIsCM <61559771+MrIsCM@users.noreply.github.com>
2025-04-17 10:05:11 +0200

    Optimized attributes and removed unnecessary functions

--- IsingClass.py
+++ IsingClass.py
@@ -32,28 +32,15 @@ class IsingModel:
     def inicia_red(self, valores=[1, -1], padding_width=1, mode='wrap'):
         """
         Inicializa la red (lattice) para la simulación del modelo de Ising.
-        Parámetros:
-            valores (list, opcional): Una lista de posibles valores de espín para la red. 
+        - Parámetros:
+            - valores (list, opcional): Una lista de posibles valores de espín para la red. 
                           Por defecto es [1, -1].
-            padding_width (int, opcional): El ancho del padding que se añadirá alrededor de la red. 
-                           Por defecto es 1.
-            mode (str, opcional): El modo utilizado para el padding. Por defecto es 'wrap', que 
-                      envuelve los bordes de la red para crear condiciones de frontera periódicas.
-        Atributos:
-            web (numpy.ndarray): La red con padding utilizada para la simulación.
+        - Atributos:
+            - web (numpy.ndarray): La red utilizada para la simulación.
         """
         rng = np.random.default_rng(self.SEED)
-        red = rng.choice(valores, size=(self.N, self.N))
-        self.web = np.pad(red, pad_width=padding_width, mode=mode)
+        self.web = rng.choice(valores, size=(self.N, self.N))
 
-
-    def actualizar_contornos(self):
-        """
-        Actualiza los contornos de la red para mantener las condiciones de frontera periódicas.
-        """
-        s = self.web[1:-1, 1:-1]
-        # Actualiza los bordes de la red
-        self.web = np.pad(s, pad_width=1, mode='wrap')
 
     def calcular_probabilidad(self, i, j):
         """
@@ -66,24 +53,23 @@ class IsingModel:
         """
         # Extrae la red de espines
         s = self.web
+        N = self.N
 
-        # Suma de vecinos inmediatos
-        vecinos_inmediatos = s[i+1, j] + s[i-1, j] + s[i, j+1] + s[i, j-1]
-        # Suma de vecinos diagonales
-        vecinos_diagonales = s[i+1, j+1] + s[i-1, j+1] + s[i+1, j-1] + s[i-1, j-1]
+        # Calcula los índices de los vecinos inmediatos y diagonales
+        # usando algebra modular para el contorno envolvente
+        vecinos_inmediatos = (
+            s[(i+1)%N, j] + s[(i-1)%N, j] +
+            s[i, (j+1)%N] + s[i, (j-1)%N]
+        )
+        vecinos_diagonales = (
+            s[(i+1)%N, (j+1)%N] + s[(i-1)%N, (j+1)%N] +
+            s[(i+1)%N, (j-1)%N] + s[(i-1)%N, (j-1)%N]
+        )
 
         # Calculo del cambio de energía
         E = 2 * s[i, j] * (self.J1 * vecinos_inmediatos + self.J2 * vecinos_diagonales)
 
         return min(1, np.exp(-E / self.T))
-    
-    def extraer_red(self):
-        """
-        Extrae la red sin el padding.
-        Devuelve:
-            numpy.ndarray: La red sin el padding.
-        """
-        return self.web[1:-1, 1:-1]
 
 
     def simular(self, pasos_MC=1000, generar_gif=False, pasos_muestreo_frames=100, generar_imagenes=True, n_imagenes=5, espaciado_imagenes='lineal', calcular_magnetizacion=True, pasos_muestreo_magnetizacion=100):
@@ -120,6 +106,11 @@ class IsingModel:
         if generar_imagenes:
             self.imagenes = np.empty((n_imagenes, self.N, self.N), dtype=int)
             self.pasos_imagenes = np.empty(n_imagenes, dtype=int)
+            if espaciado_imagenes == 'lineal':
+                pasos_imagenes = np.linspace(0, pasos_MC, n_imagenes, dtype=int)
+            elif espaciado_imagenes == 'logaritmico':
+                pasos_imagenes = np.logspace(0, pasos_MC, n_imagenes, dtype=int)
+            aux_idx = 0
         else:
             self.imagenes = None
             self.pasos_imagenes = None
@@ -129,28 +120,25 @@ class IsingModel:
         for paso in range(pasos_MC):
             # Guarda el estado inicial
             if generar_gif and paso % pasos_muestreo_frames == 0:
-                self.frames[paso // pasos_muestreo_frames] = self.extraer_red()
+                self.frames[paso // pasos_muestreo_frames] = self.web.copy()
                 self.pasos_frames[paso // pasos_muestreo_frames] = paso
 
             if calcular_magnetizacion and paso % pasos_muestreo_magnetizacion == 0:
-                magnetizacion = np.abs(np.sum(self.extraer_red()) / (self.N * self.N))
+                magnetizacion = np.abs(np.sum(self.web) / (self.N * self.N))
                 self.magnetizaciones[paso // pasos_muestreo_magnetizacion] = magnetizacion
                 self.pasos_magnetizaciones[paso // pasos_muestreo_magnetizacion] = paso
 
             if generar_imagenes:
-                if espaciado_imagenes == 'lineal':
-                    if paso % (pasos_MC // n_imagenes) == 0:
-                        self.imagenes[paso // (pasos_MC // n_imagenes)] = self.extraer_red()
-                        self.pasos_imagenes[paso // (pasos_MC // n_imagenes)] = paso
-                elif espaciado_imagenes == 'logaritmico':
-                    if paso % (int(pasos_MC / (np.log10(paso + 1)))) == 0:
-                        self.imagenes[paso // (int(pasos_MC / (np.log10(paso + 1))))] = self.extraer_red()
-                        self.pasos_imagenes[paso // (int(pasos_MC / (np.log10(paso + 1))))] = paso
+                if paso in pasos_imagenes:
+                    self.imagenes[aux_idx] = self.web.copy()
+                    self.pasos_imagenes[aux_idx] = paso
+                    aux_idx += 1
+        
             
             # Genera los numeros aleatorios para TODO el paso Monte Carlo
             # Mejora significativamente el rendimiento
-            i_idx = rng.integers(1, self.N + 1, size=self.N * self.N)
-            j_idx = rng.integers(1, self.N + 1, size=self.N * self.N)
+            i_idx = rng.integers(0, self.N, size=self.N * self.N)
+            j_idx = rng.integers(0, self.N, size=self.N * self.N)
 
             # Genera tambien los valores de aceptacion para cada espin
             aceptacion = rng.random(self.N * self.N)
@@ -165,51 +153,51 @@ class IsingModel:
                     # Cambia el espín
                     self.web[i, j] *= -1
 
-            # Actualiza las condiciones de contorno una vez cada paso Monte Carlo
-            self.actualizar_contornos()
-
         # Guardar (si necesario) el estado final
 
         if generar_gif and (len(self.frames) == 0 or self.pasos_frames[-1] != pasos_MC):
-            self.frames[-1] = self.extraer_red()
+            self.frames[-1] = self.web.copy()
             self.pasos_frames[-1] = pasos_MC
 
         if calcular_magnetizacion and (len(self.magnetizaciones) == 0 or self.pasos_magnetizaciones[-1] != pasos_MC):
-            magnetizacion = np.abs(np.sum(self.extraer_red()) / (self.N * self.N))
+            magnetizacion = np.abs(np.sum(self.web) / (self.N * self.N))
             self.magnetizaciones[-1] = magnetizacion
             self.pasos_magnetizaciones[-1] = pasos_MC
 
         if generar_imagenes and (len(self.imagenes) == 0 or self.pasos_imagenes[-1] != pasos_MC):
-            self.imagenes[-1] = self.extraer_red()
+            self.imagenes[-1] = self.web.copy()
             self.pasos_imagenes[-1] = pasos_MC
 
     def configurar_paths(self, gif_dir='gifs', imagenes_dir='images', magnetizaciones_dir='magnetizaciones'):
         """
         Configura los directorios para guardar los resultados de la simulación.
-        Parámetros:
-            gif_dir (str, opcional): El directorio para guardar los gifs. Por defecto es 'gifs'.
-            imagenes_dir (str, opcional): El directorio para guardar las imágenes. Por defecto es 'images'.
-            magnetizaciones_dir (str, opcional): El directorio para guardar las magnetizaciones. Por defecto es 'magnetizaciones'.
+        - Parámetros:
+            - gif_dir (str, opcional): El directorio para guardar los gifs. Por defecto es 'gifs'.
+            - imagenes_dir (str, opcional): El directorio para guardar las imágenes. Por defecto es 'images'.
+            - magnetizaciones_dir (str, opcional): El directorio para guardar las magnetizaciones. Por defecto es 'magnetizaciones'.
         """
 
         if self.test_mode == True:
-            test_dir = Path('test')
-            test_dir.mkdir(exist_ok=True)
+            test_dir = Path(f'test_N{self.N}_T{self.T}')
+            test_dir.mkdir(exist_ok=True, parents=True)
 
             gif_dir = test_dir / gif_dir
-            gif_dir.mkdir(exist_ok=True)
+            gif_dir.mkdir(exist_ok=True, parents=True)
             imagenes_dir = test_dir / imagenes_dir
-            imagenes_dir.mkdir(exist_ok=True)
+            imagenes_dir.mkdir(exist_ok=True, parents=True)
             magnetizaciones_dir = test_dir / magnetizaciones_dir
-            magnetizaciones_dir.mkdir(exist_ok=True)
+            magnetizaciones_dir.mkdir(exist_ok=True, parents=True)
 
         else:
-            gif_dir = Path(gif_dir)
-            gif_dir.mkdir(exist_ok=True)
-            imagenes_dir = Path(imagenes_dir)
-            imagenes_dir.mkdir(exist_ok=True)
-            magnetizaciones_dir = Path(magnetizaciones_dir)
-            magnetizaciones_dir.mkdir(exist_ok=True)
+            resultados_dir = Path(f'resultados_N{self.N}_T{self.T}')
+            resultados_dir.mkdir(exist_ok=True, parents=True)
+            gif_dir = resultados_dir / gif_dir
+            gif_dir.mkdir(exist_ok=True, parents=True)
+            imagenes_dir = resultados_dir / imagenes_dir
+            imagenes_dir.mkdir(exist_ok=True, parents=True)
+            magnetizaciones_dir = resultados_dir / magnetizaciones_dir
+            magnetizaciones_dir.mkdir(exist_ok=True, parents=True)
+            
 
         self.gif_dir = gif_dir
         self.imagenes_dir = imagenes_dir
